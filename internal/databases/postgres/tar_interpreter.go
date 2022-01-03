9b37f61d20dc6d277c088b7afd817ec904f243ac
Philip Dubé <phdub@microsoft.com>
2022-01-03 20:57:20 +0000

    Remove tar interpreter unwrapRegularFileOld

--- internal/databases/postgres/tar_interpreter.go
+++ internal/databases/postgres/tar_interpreter.go
@@ -12,7 +12,6 @@ import (
 	"github.com/spf13/viper"
 	"github.com/wal-g/tracelog"
 	"github.com/wal-g/wal-g/internal"
-	"github.com/wal-g/wal-g/utility"
 )
 
 // FileTarInterpreter extracts input to disk.
@@ -34,61 +33,6 @@ func NewFileTarInterpreter(
 		filesToUnwrap, newUnwrapResult(), createNewIncrementalFiles}
 }
 
-// TODO : unit tests
-func (tarInterpreter *FileTarInterpreter) unwrapRegularFileOld(fileReader io.Reader,
-	fileInfo *tar.Header,
-	targetPath string,
-	fsync bool) error {
-	if tarInterpreter.FilesToUnwrap != nil {
-		if _, ok := tarInterpreter.FilesToUnwrap[fileInfo.Name]; !ok {
-			// don't have to unwrap it this time
-			tracelog.DebugLogger.Printf("Don't have to unwrap '%s' this time\n", fileInfo.Name)
-			return nil
-		}
-	}
-	fileDescription, haveFileDescription := tarInterpreter.FilesMetadata.Files[fileInfo.Name]
-
-	// If this file is incremental we use it's base version from incremental path
-	if haveFileDescription && tarInterpreter.Sentinel.IsIncremental() && fileDescription.IsIncremented {
-		err := ApplyFileIncrement(targetPath, fileReader, tarInterpreter.createNewIncrementalFiles, fsync)
-		return errors.Wrapf(err, "Interpret: failed to apply increment for '%s'", targetPath)
-	}
-	err := PrepareDirs(fileInfo.Name, targetPath)
-	if err != nil {
-		return errors.Wrap(err, "Interpret: failed to create all directories")
-	}
-	file, err := os.OpenFile(targetPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		return errors.Wrapf(err, "failed to create new file: '%s'", targetPath)
-	}
-
-	_, err = io.Copy(file, fileReader)
-	if err != nil {
-		err1 := file.Close()
-		if err1 != nil {
-			tracelog.ErrorLogger.Printf("Interpret: failed to close file '%s' because of error: %v", targetPath, err1)
-		}
-		err1 = os.Remove(targetPath)
-		if err1 != nil {
-			tracelog.ErrorLogger.Fatalf("Interpret: failed to remove file '%s' because of error: %v", targetPath, err1)
-		}
-		return errors.Wrap(err, "Interpret: copy failed")
-	}
-	defer utility.LoggedClose(file, "")
-
-	mode := os.FileMode(fileInfo.Mode)
-	if err = os.Chmod(file.Name(), mode); err != nil {
-		return errors.Wrap(err, "Interpret: chmod failed")
-	}
-
-	if fsync {
-		err = file.Sync()
-		return errors.Wrap(err, "Interpret: fsync failed")
-	}
-
-	return nil
-}
-
 // Interpret extracts a tar file to disk and creates needed directories.
 // Returns the first error encountered. Calls fsync after each file
 // is written successfully.
@@ -98,11 +42,7 @@ func (tarInterpreter *FileTarInterpreter) Interpret(fileReader io.Reader, fileIn
 	fsync := !viper.GetBool(internal.TarDisableFsyncSetting)
 	switch fileInfo.Typeflag {
 	case tar.TypeReg, tar.TypeRegA:
-		// temporary switch to determine if new unwrap logic should be used
-		if useNewUnwrapImplementation {
-			return tarInterpreter.unwrapRegularFileNew(fileReader, fileInfo, targetPath, fsync)
-		}
-		return tarInterpreter.unwrapRegularFileOld(fileReader, fileInfo, targetPath, fsync)
+		return tarInterpreter.unwrapRegularFileNew(fileReader, fileInfo, targetPath, fsync)
 	case tar.TypeDir:
 		err := os.MkdirAll(targetPath, 0755)
 		if err != nil {
